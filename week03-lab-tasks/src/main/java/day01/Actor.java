8d146710e3b810ebbe6295a6191364420a83310a
Keltaisten <keltaisten@gmail.com>
2021-11-02 15:52:11 +0100

    Movie

---
+++ week03-lab-tasks/src/main/java/day01/Actor.java
@@ -0,0 +1,20 @@
+package day01;
+
+public class Actor {
+    private String name;
+    private int ageOfBirth;
+
+    public Actor(String name, int ageOfBirth) {
+        this.name = name;
+        this.ageOfBirth = ageOfBirth;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAgeOfBirth() {
+        return ageOfBirth;
+    }
+
+}
