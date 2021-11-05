393146bd9e5ea5aa4f8d5a23eb02156aabd356d2
Keltaisten <keltaisten@gmail.com>
2021-11-05 17:08:57 +0100

    Stock updated again

--- week04-lab-tasks/src/main/java/day03/Stock.java
+++ week04-lab-tasks/src/main/java/day03/Stock.java
@@ -18,17 +18,22 @@ public class Stock {
         for(int u = 0; u<doubleList.size();u++){
             if(firstN > doubleList.get(u)){
 
-            for(int k = u +1; k<doubleList.size();k++){
-                double diff = doubleList.get(k)-doubleList.get(u);
-                steps++;
-                if(diff>numberMaxdiff){
-                    numberMaxdiff = diff;
+                for(int k = u +1; k<doubleList.size();k++){
+                    if(secondN < doubleList.get(k)){
+
+                    double diff = doubleList.get(k)-doubleList.get(u);
+                    steps++;
+                    if(diff>numberMaxdiff){
+                        numberMaxdiff = diff;
+                    }
+                    }else{
+                        secondN = doubleList.get(k);
+                    }
                 }
-            }
 
             }
             else{
-            firstN = doubleList.get(u);
+                firstN = doubleList.get(u);
             }
         }
         System.out.println(steps);
@@ -39,7 +44,28 @@ public class Stock {
         Stock stock = new Stock(Arrays.asList(1.23,4.35,0.23,4.0,8.4));
         System.out.println(stock.maxProfit());
 
-        Stock stock2 = new Stock(Arrays.asList(12.0,2.0,8.0,4.0));
+        Stock stock2 = new Stock(Arrays.asList(12.0,2.0,8.0,4.0,1.23,12.0, 4.35,0.23,4.0,8.4));
         System.out.println(stock2.maxProfit());
     }
+
+
+//    public double maxProfit(){
+//        double numberMaxdiff = 0.0;
+//        double firstN = 0.0;
+//        double secondN = 0.0;
+//        int steps = 0;
+//        for(int u = 0; u<doubleList.size();u++){
+//
+//            for(int k = u +1; k<doubleList.size();k++){
+//                double diff = doubleList.get(k)-doubleList.get(u);
+//                steps++;
+//                if(diff>numberMaxdiff){
+//                    numberMaxdiff = diff;
+//                }
+//            }
+//
+//        }
+//        System.out.println(steps);
+//        return numberMaxdiff;
+//    }
 }
