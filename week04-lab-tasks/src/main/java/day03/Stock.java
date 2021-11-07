e3cfbba17b55e111f5a113cf7e522d8f7c5c0cf8
Keltaisten <keltaisten@gmail.com>
2021-11-07 22:28:35 +0100

    Stock new update - separate methods

--- week04-lab-tasks/src/main/java/day03/Stock.java
+++ week04-lab-tasks/src/main/java/day03/Stock.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 public class Stock {
     private List<Double> doubleList;
+    private int steps = 0;
 
     public Stock(List<Double> doubleList) {
         this.doubleList = doubleList;
@@ -14,29 +15,30 @@ public class Stock {
         double numberMaxdiff = 0.0;
         double firstN = 0.0;
         double secondN = 0.0;
-        int steps = 0;
         for(int u = 0; u<doubleList.size();u++){
             if(firstN > doubleList.get(u)){
-
-                for(int k = u +1; k<doubleList.size();k++){
-                    if(secondN < doubleList.get(k)){
-
-                    double diff = doubleList.get(k)-doubleList.get(u);
-                    steps++;
-                    if(diff>numberMaxdiff){
-                        numberMaxdiff = diff;
-                    }
-                    }else{
-                        secondN = doubleList.get(k);
-                    }
-                }
-
+                numberMaxdiff = checkDailyDifferences(numberMaxdiff,u,secondN);
             }
             else{
                 firstN = doubleList.get(u);
             }
         }
         System.out.println(steps);
+        return numberMaxdiff;
+    }
+
+    public double checkDailyDifferences(double numberMaxdiff, int u, double secondN){
+        for(int k = u +1; k<doubleList.size();k++){
+            if(secondN < doubleList.get(k)){
+                double diff = doubleList.get(k)-doubleList.get(u);
+                steps++;
+                if(diff>numberMaxdiff){
+                    numberMaxdiff = diff;
+                }
+            }else{
+                secondN = doubleList.get(k);
+            }
+        }
         return numberMaxdiff;
     }
 
@@ -47,25 +49,4 @@ public class Stock {
         Stock stock2 = new Stock(Arrays.asList(12.0,2.0,8.0,4.0,1.23,12.0, 4.35,0.23,4.0,8.4));
         System.out.println(stock2.maxProfit());
     }
-
-
-//    public double maxProfit(){
-//        double numberMaxdiff = 0.0;
-//        double firstN = 0.0;
-//        double secondN = 0.0;
-//        int steps = 0;
-//        for(int u = 0; u<doubleList.size();u++){
-//
-//            for(int k = u +1; k<doubleList.size();k++){
-//                double diff = doubleList.get(k)-doubleList.get(u);
-//                steps++;
-//                if(diff>numberMaxdiff){
-//                    numberMaxdiff = diff;
-//                }
-//            }
-//
-//        }
-//        System.out.println(steps);
-//        return numberMaxdiff;
-//    }
 }
